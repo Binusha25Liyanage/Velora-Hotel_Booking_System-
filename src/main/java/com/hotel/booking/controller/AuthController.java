87c21f666ec9cf652d90956e79221796e17dca46
Binusha Liyanage <binusha45liyanage@gmail.com>
2026-02-08 00:57:44 +0530

    Add AuthController and Hotel model classes

---
+++ src/main/java/com/hotel/booking/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.hotel.booking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.hotel.booking.model.User;
+import com.hotel.booking.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+
+        user.setRole("USER");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+}
---
+++ src/main/java/com/hotel/booking/model/Hotel.java
@@ -0,0 +1,32 @@
+package com.hotel.booking.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "hotels")
+public class Hotel {
+
+    @Id
+    private String id;
+    private String name;
+    private String location;
+    private String description;
+    private double rating;
+
+    public Hotel() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+}
